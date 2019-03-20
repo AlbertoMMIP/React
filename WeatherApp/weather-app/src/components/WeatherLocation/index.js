38b1daf51c82f253eb683a923fdd5301d559d968
Albert <alberto@email.com>
2019-03-20 14:24:29 -0600

    Fetch, promises and dep convert-units

--- WeatherApp/weather-app/src/components/WeatherLocation/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/index.js
@@ -1,22 +1,22 @@
 import React,{Component} from 'react';
+import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css'
 import {
-    SUN, WINDY,
+    SUN,
 } from './../../constants/weather';
+const location = 'Mexico,mx';
+const api_key = 'f99bbd9e4959b513e9bd0d7f7356b38d';
+const base_url = 'https://api.openweathermap.org/data/2.5/weather';
+
+const api_weather = `${base_url}?q=${location}&appid=${api_key}`;
 
 const data = {
     temperature:5,
     weatherState:SUN,
     humidity:10,
     wind:"10 m/S"
-}
-const data2 = {
-    temperature:15,
-    weatherState:WINDY,
-    humidity:20,
-    wind:"40 m/S"
 }
 
 class WeatherLocation extends  Component {
@@ -29,9 +29,42 @@ class WeatherLocation extends  Component {
         };
     }
 
+    getTemp = kelvin => {
+        return convert(kelvin).from("K").to("C").toFixed(2);
+    };
+
+    getWeatherState = weather_data =>{
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const {humidity, temp} = weather_data.main;
+        const {speed} = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+        const temperature = this.getTemp(temp);
+        const data = {
+            humidity,
+            temperature,
+            weatherState,
+            wind: `${speed} m/s`
+        }
+
+        return data;
+    }
+
     handleUpdateClick = () => {
-        console.log('Actualizado')
-        this.setState({city:'Peru',data:data2})
+        fetch(api_weather)
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                const newWeather = this.getData(data);
+                console.log(newWeather)
+                //debugger; 
+                this.setState({data:newWeather})
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     render(){
