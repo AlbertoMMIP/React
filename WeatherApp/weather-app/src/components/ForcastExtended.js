e212e175561b1d9e537d50ae90ca36bd548e476f
originacion <originacion@localhost.localdomain>
2019-04-11 16:01:33 -0500

    ForecastItem

--- WeatherApp/weather-app/src/components/ForcastExtended.js
+++ WeatherApp/weather-app/src/components/ForcastExtended.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import ForcastItem from './ForcastItem';
 import './Styles.css';
 
+
+const days = ['Lunes','Martes','Miercoles','Jueves','Viernes'];
+const data = {
+  temperature:0,
+  weatherState:'',
+  humidity: 0,
+  wind:''
+
+}
 class ForcastExtended extends Component {
+
+  renderForcastItemDays() {
+    return days.map(day => <ForcastItem key={day} weekDay={day} hour={10} data={data} />);
+  }
+
   render(){
     const {city} = this.props;
     return(
       <div>
         <h2 className='forecast-title' >Pronósticoo extendido para {city}</h2>
+        {this.renderForcastItemDays() }
       </div>
     );
   }
