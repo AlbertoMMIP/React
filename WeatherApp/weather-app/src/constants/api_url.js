3fd495a702d48193e6c8ad2a2d1089b2a3e954af
Alberto Martinez <alberto@intelligential.tech>
2021-06-26 09:49:10 -0500

    Change app repo

--- WeatherApp/weather-app/src/constants/api_url.js
+++
@@ -1,3 +0,0 @@
-export const api_key = 'f99bbd9e4959b513e9bd0d7f7356b38d';
-export const base_url = 'https://api.openweathermap.org/data/2.5/weather';
-export const base_url_forecast = 'https://api.openweathermap.org/data/2.5/forecast';
